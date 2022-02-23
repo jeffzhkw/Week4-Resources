56d0c487147247598388c7f874275b1c32ebb502
Siddharth Sagar <ss13973@nyu.edu>
2022-02-23 17:36:09 -0500

    Enemies can Circle the players

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     public float speed;
     public GameObject Player;
     public float ClosestDistance = 4f;
+    public float CirclePlayer = 0;  //-1 for clockwise, 0 for none, 1 for anticlockwise
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,7 @@ public class Move : MonoBehaviour
     void Update()
     {
         if( (Player.transform.position- this.transform.position).magnitude < ClosestDistance){
+            this.transform.position += Vector3.Cross(Vector3.forward,getDirectionToPlayer())*CirclePlayer;
             return;
         }
         Debug.Log( Player.transform.position.magnitude - this.transform.position.magnitude);
