d0a560129a4e80922349c249ba599d4b93b779ee
kc3753 <47250615+kc3753@users.noreply.github.com>
2022-02-24 14:48:19 -0500

    waves

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -9,20 +9,47 @@ public class EnemySystem : MonoBehaviour
     public int xpos;
     public int ypos;
     public static int enemyCount;
+    public float timeBetweenWaves;
+    private float timeRemaining;
+    private int waveNumber = 0;
     void Start()
     {
-        StartWave();
+        timeRemaining = timeBetweenWaves;
+        StartWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemyCreate();
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        enemyCount = enemies.Length;
+        if(enemyCount < 1){
+            
+            timeRemaining -= Time.deltaTime;
+            if(timeRemaining < 0){
+                waveNumber +=1;
+                StartWave(waveNumber);
+                timeRemaining = timeBetweenWaves;
+            }
+        }
+        
     }
 
-    void StartWave() {
+    void StartWave(int wave) {
         Debug.Log("Start Wave");
-        EnemyCreate();
+        switch(wave){
+            case 0:
+                EnemyCreate();
+                break;
+            case 1:
+                EnemyCreate1();
+                break;
+            case 2:
+                EnemyCreate2();
+                break;
+            default:
+                break;
+        }
 
     }
 
@@ -31,6 +58,38 @@ public class EnemySystem : MonoBehaviour
             xpos = Random.Range(-17,18);
             ypos = Random.Range(-9,10);
             Instantiate(Enemy0, new Vector3(xpos,ypos,0), Enemy0.transform.rotation);
+            xpos = Random.Range(-17,18);
+            ypos = Random.Range(-9,10);
+            Instantiate(Enemy1, new Vector3(xpos,ypos,0), Enemy1.transform.rotation);
+            xpos = Random.Range(-17,18);
+            ypos = Random.Range(-9,10);
+            Instantiate(Enemy2, new Vector3(xpos,ypos,0), Enemy2.transform.rotation);
+            enemyCount +=3;
+        }
+    }
+
+    void EnemyCreate1(){
+        while(enemyCount < 9){
+            xpos = Random.Range(-17,18);
+            ypos = Random.Range(-9,10);
+            Instantiate(Enemy0, new Vector3(xpos,ypos,0), Enemy0.transform.rotation);
+            xpos = Random.Range(-17,18);
+            ypos = Random.Range(-9,10);
+            Instantiate(Enemy1, new Vector3(xpos,ypos,0), Enemy1.transform.rotation);
+            enemyCount +=3;
+        }
+    }
+     void EnemyCreate2(){
+        while(enemyCount < 4){
+            xpos = Random.Range(-17,18);
+            ypos = Random.Range(-9,10);
+            Instantiate(Enemy2, new Vector3(xpos,ypos,0), Enemy2.transform.rotation);
+            enemyCount +=1;
+        }
+        while(enemyCount < 2){
+            xpos = Random.Range(-17,18);
+            ypos = Random.Range(-9,10);
+            Instantiate(Enemy1, new Vector3(xpos,ypos,0), Enemy1.transform.rotation);
             enemyCount +=1;
         }
     }
